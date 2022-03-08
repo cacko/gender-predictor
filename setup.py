7a4956c174158f0b8a31b490211235d9a7694b9e
cacko <alex@cacko.net>
2022-03-08 12:04:48 +0000

    My boss forced me to build this feature... Pure shit.

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(name='gender_predictor',
-      version='1.0.1',
+      version='1.0.2',
       description=(' '),
       author='Clint Valentine',
       author_email='valentine.clint@gmail.com',
       url='https://github.com/clintval/gender_predictor',
+      package_data={'': ['names.pickle']},
       packages=find_packages(),
       install_requires=['nltk'],
       )
